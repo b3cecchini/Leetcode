66da181ce42bb247366c772b80b21a97186b3462
b3cecchini <b3cecchini@gmail.com>
2024-11-09 02:04:18 -0500

    75

---
+++ Problems/75.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems
+{
+    public class _75
+    {
+        public void SortColors(int[] nums)
+        {
+            int red = 0, white = 0, blue = 0;
+
+            foreach (int i in nums)
+            {
+                if (i == 0) red++;
+                else if (i == 1) white++;
+                else blue++;
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(red > 0)
+                {
+                    nums[i] = 0;
+                    red--;
+                }
+                else if(white > 0)
+                {
+                    nums[i] = 1;
+                    white--;
+                }
+                else
+                {
+                    nums[i] = 2;
+                    blue--;
+                }
+            }
+        }
+    }
+}
---
+++ Problems/99.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leetcode.Utils;
+
+namespace Leetcode.Problems
+{
+    public class _99
+    {
+        public void RecoverTree(TreeNode root)
+        {
+            if (root == null) return;
+            if(dfs(root).val > root.val)
+            {
+
+            };
+        }
+
+        private TreeNode dfs(TreeNode root)
+        {
+            if (root == null) return null;
+            if(dfs(root.left) != null && root.left.val > root.val) 
+            {
+                int temp = root.val;
+                root.val = root.left.val;
+                root.left.val = temp;
+            }
+        }
+    }
+}
